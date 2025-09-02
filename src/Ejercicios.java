84d827ade5f68acafb62b466249a25ba805f28d8
Icahzzz <isalflo31@gmail.com>
2025-09-02 17:48:03 -0600

    Ejercicio 1

---
+++ src/Ejercicios.java
@@ -0,0 +1,13 @@
+import javax.swing.*;
+
+public class Ejercicios {
+    private JTextField textField1;
+    private JTextField textField3;
+    private JButton cancelarButton;
+    private JButton guardarButton;
+    private JPanel panel1;
+
+    private void createUIComponents() {
+        // TODO: place custom component creation code here
+    }
+}

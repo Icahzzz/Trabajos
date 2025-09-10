9107faa6d40cc3a28a4e4d74cc24601988bc3cf8
Icahzzz <isalflo31@gmail.com>
2025-09-10 13:09:15 -0600

    Se agrego la tarea del dia 08 de septiembre (login)

---
+++ src/Ejemplo3.java
@@ -0,0 +1,11 @@
+import javax.swing.*;
+
+public class Ejemplo3 {
+    private JPanel panelPrincipal;
+    private JPanel panelSuperior;
+    private JPanel panelIzquierdo;
+
+    private void createUIComponents() {
+        // TODO: place custom component creation code here
+    }
+}
---
+++ src/InicioDeSesion.java
@@ -0,0 +1,11 @@
+import javax.swing.*;
+
+public class InicioDeSesion {
+    private JPanel inicioSesion;
+    private JPanel logoyTitulo;
+    private JPanel escribir;
+    private JTextField correoOUsuarioTextField;
+    private JTextField contraseñaTextField;
+    private JCheckBox recuerdameCheckBox;
+    private JButton iniciarSesionButton;
+}
